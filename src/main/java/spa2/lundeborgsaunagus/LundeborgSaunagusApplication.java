ddb20526d6925bda49cece5e50f0b040deca572f
Frede-72 <frjo0005@stud.ek.dk>
2026-05-04 10:59:35 +0200

    Setup spring project

---
+++ src/main/java/spa2/lundeborgsaunagus/LundeborgSaunagusApplication.java
@@ -0,0 +1,13 @@
+package spa2.lundeborgsaunagus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LundeborgSaunagusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LundeborgSaunagusApplication.class, args);
+    }
+
+}
