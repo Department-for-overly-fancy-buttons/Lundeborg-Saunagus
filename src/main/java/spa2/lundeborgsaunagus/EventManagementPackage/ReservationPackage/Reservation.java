f93673cad5130cc6ba87a09295160dadf1e21882
Frede-72 <frjo0005@stud.ek.dk>
2026-05-15 11:11:22 +0200

    REMOVED: reservation

--- src/main/java/spa2/lundeborgsaunagus/EventManagementPackage/ReservationPackage/Reservation.java
+++ src/main/java/spa2/lundeborgsaunagus/EventManagementPackage/ReservationPackage/Reservation.java
@@ -1,48 +1,48 @@
-package spa2.lundeborgsaunagus.EventManagementPackage.ReservationPackage;
-
-import jakarta.persistence.*;
-import spa2.lundeborgsaunagus.EventManagementPackage.EventPackage.Event;
-import spa2.lundeborgsaunagus.TicketPackage.Ticket;
-import spa2.lundeborgsaunagus.UserPackage.GusUser;
-
-@Entity
-public class Reservation {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false)
-    private Long ticketId;
-    @Column(nullable = false)
-    private Long eventId;
-
-    public Reservation(Long ticketId, Long eventId) {
-        this.ticketId = ticketId;
-        this.eventId = eventId;
-    }
-
-    public Reservation(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTicketId() {
-        return ticketId;
-    }
-
-    public void setTicketId(Long ticketId) {
-        this.ticketId = ticketId;
-    }
-
-    public Long getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Long eventId) {
-        this.eventId = eventId;
-    }
-}
+//package spa2.lundeborgsaunagus.EventManagementPackage.ReservationPackage;
+//
+//import jakarta.persistence.*;
+//import spa2.lundeborgsaunagus.EventManagementPackage.EventPackage.Event;
+//import spa2.lundeborgsaunagus.TicketPackage.Ticket;
+//import spa2.lundeborgsaunagus.UserPackage.GusUser;
+//
+//@Entity
+//public class Reservation {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    @Column(nullable = false)
+//    private Long ticketId;
+//    @Column(nullable = false)
+//    private Long eventId;
+//
+//    public Reservation(Long ticketId, Long eventId) {
+//        this.ticketId = ticketId;
+//        this.eventId = eventId;
+//    }
+//
+//    public Reservation(){}
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getTicketId() {
+//        return ticketId;
+//    }
+//
+//    public void setTicketId(Long ticketId) {
+//        this.ticketId = ticketId;
+//    }
+//
+//    public Long getEventId() {
+//        return eventId;
+//    }
+//
+//    public void setEventId(Long eventId) {
+//        this.eventId = eventId;
+//    }
+//}
--- src/main/java/spa2/lundeborgsaunagus/EventManagementPackage/ReservationPackage/ReservationRepository.java
+++ src/main/java/spa2/lundeborgsaunagus/EventManagementPackage/ReservationPackage/ReservationRepository.java
@@ -1,11 +1,11 @@
-package spa2.lundeborgsaunagus.EventManagementPackage.ReservationPackage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-
-    List<Reservation> findAllByEventId(Long eventId);
-
-}
+//package spa2.lundeborgsaunagus.EventManagementPackage.ReservationPackage;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+//
+//    List<Reservation> findAllByEventId(Long eventId);
+//
+//}
