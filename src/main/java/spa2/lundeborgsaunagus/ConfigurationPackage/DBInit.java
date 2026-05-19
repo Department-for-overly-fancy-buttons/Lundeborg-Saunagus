495d01f68aec7e8656e4a168c4907533ffc73523
Frede-72 <frjo0005@stud.ek.dk>
2026-05-19 15:51:34 +0200

    test data

--- src/main/java/spa2/lundeborgsaunagus/ConfigurationPackage/DBInit.java
+++ src/main/java/spa2/lundeborgsaunagus/ConfigurationPackage/DBInit.java
@@ -36,14 +36,14 @@ public class DBInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-    GusUser user = new GusUser("Freja", passwordEncoder.encode("test"), "Freja", "Johannessen", "11111111", "vej 0 b", LocalDate.of(2000, 2, 2), Gender.FEMALE, Role.EMPLOYEE);
+        GusUser user = new GusUser("Freja", passwordEncoder.encode("test"), "Freja", "Johannessen", "11111111", "vej 0 b", LocalDate.of(2000, 2, 2), Gender.FEMALE, Role.EMPLOYEE);
         GusUser user2 = new GusUser("Freja2", passwordEncoder.encode("test"), "Freja", "Johannesse", "11111112", "vej 0 b", LocalDate.of(1902, 1, 1), Gender.FEMALE, Role.CUSTOMER);
         GusUser user3 = new GusUser("Freja3", passwordEncoder.encode("test"), "Freja", "Johannessensen", "11111113", "vej 0 b", LocalDate.of(1992, 10, 10), Gender.FEMALE, Role.ADMIN);
 
         userRepository.save(user);
         userRepository.save(user2);
         userRepository.save(user3);
-        Event event = new Event(LocalDate.now(), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna");
+        Event event = new Event(LocalDate.now(), LocalTime.now().plusMinutes(5), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna");
         event.setInformation("kun for over 90");
         Event event2 = new Event(LocalDate.now().plusDays(10), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna2");
         event2.setInformation("Hunde tilladt");
@@ -59,11 +59,14 @@ public class DBInit implements CommandLineRunner {
         Event event10 = new Event(LocalDate.now().plusDays(33), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Saun12");
         Event event11 = new Event(LocalDate.now().plusDays(44), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna22");
         Event event12 = new Event(LocalDate.now().plusDays(40), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna42");
+        Event event13 = new Event(LocalDate.now().minusDays(33), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Saun12");
+        Event event14 = new Event(LocalDate.now().minusDays(44), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna22");
+        Event event15 = new Event(LocalDate.now().minusDays(40), LocalTime.now(), LocalTime.now().plusHours(2), user, "fyn", 30, "Sauna42");
 
 
         Ticket ticket = new Ticket(user2, "ticket", false, 50, LocalDateTime.now(), event);
         Ticket ticket2 = new Ticket(user, "ticket", true, 50, LocalDateTime.now(), event);
-        Ticket ticket3 = new Ticket(user3, "ticket", false, 500, LocalDateTime.now(), event);
+        Ticket ticket3 = new Ticket(user3, "ticket", false, 500, LocalDateTime.now(), event8);
         Ticket ticket4 = new Ticket(user2, "ticket", false, 50, LocalDateTime.now(), event2);
         Ticket ticket5 = new Ticket(user, "ticket", true, 50, LocalDateTime.now(), event2);
         Ticket ticket6 = new Ticket(user3, "ticket", false, 500, LocalDateTime.now(), event3);
@@ -71,7 +74,7 @@ public class DBInit implements CommandLineRunner {
         Ticket ticket8 = new Ticket(user2, "ticket", false, 50, LocalDateTime.now(), event6);
         Ticket ticket9 = new Ticket(user, "ticket", true, 50, LocalDateTime.now(), event12);
         Ticket ticket10 = new Ticket(user3, "ticket", false, 500, LocalDateTime.now(), event9);
-        eventRepository.saveAll(List.of(event, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12));
-        ticketRepository.saveAll(List.of(ticket,ticket2,ticket3,ticket4,ticket5,ticket6,ticket7,ticket8,ticket9,ticket10));
+        eventRepository.saveAll(List.of(event, event2, event3, event4, event5, event6, event7, event8, event9, event10, event11, event12, event13, event14, event15));
+        ticketRepository.saveAll(List.of(ticket, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8, ticket9, ticket10));
     }
 }
